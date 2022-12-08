cab18858af1c0d1a9ab9b1ded0e0bad8dc30c9de
Mateus Oliveira <mateus.silva@b2ml.com.br>
2022-12-08 08:28:13 -0300

    reafactor(MethodChannelBluetoothAdapter): making MethodChannel names constants

--- android/src/main/kotlin/oliver/mat/bluetooth_classic/MethodChannelBluetoothAdapter.kt
+++ android/src/main/kotlin/oliver/mat/bluetooth_classic/MethodChannelBluetoothAdapter.kt
@@ -22,62 +22,63 @@ class MethodChannelBluetoothAdapter : FlutterPlugin, MethodCallHandler, Activity
     private val bluetoothAdapter = BluetoothAdapter()
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "bluetooth_adapter")
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, BLUETOOTH_ADAPTER)
         channel.setMethodCallHandler(this)
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        if (call.method == "initBluetoothAdapter") {
+
+        if (call.method == INIT_BLUETOOTH_ADAPTER) {
             bluetoothAdapter.initBluetoothAdapter(activity)
             result.success(true)
         }
 
-        if (call.method == "checkPermission") {
+        if (call.method == CHECK_PERMISSION) {
             result.success(CheckSelfPermission.checkVersion(activity))
         }
 
-        if (call.method == "isEnableBluetooth") {
+        if (call.method == IS_ENABLE_BLUETOOTH) {
             result.success(bluetoothAdapter.isEnableBluetooth())
         }
 
-        if (call.method == "isDiscoveryDevice") {
+        if (call.method == IS_DISCOVERY_DEVICE) {
             result.success(bluetoothAdapter.isDiscoveryDevice())
         }
 
-        if (call.method == "enableBluetooth") {
+        if (call.method == ENABLE_BLUETOOTH) {
             bluetoothAdapter.enableBluetooth(activity)
             result.success(true)
         }
 
-        if (call.method == "startDeviceDiscovery") {
+        if (call.method == START_DISCOVERY_DEVICE) {
             bluetoothAdapter.startDeviceDiscovery()
             result.success(true)
         }
 
-        if (call.method == "stopDeviceDiscovery") {
+        if (call.method == STOP_DISCOVERY_DEVICE) {
             bluetoothAdapter.stopDeviceDiscovery()
             result.success(true)
         }
 
-        if (call.method == "listNewDevices") {
+        if (call.method == LIST_NEW_DEVICES) {
             result.success(bluetoothAdapter.listNewDevices())
         }
 
-        if (call.method == "listPairedDevices") {
+        if (call.method == LIST_PAIRED_DEVICES) {
             result.success(bluetoothAdapter.listPairedDevices())
         }
 
-        if (call.method == "callPairedDevices") {
+        if (call.method == CALL_PAIRED_DEVICES) {
             bluetoothAdapter.callPairedDevices()
             result.success(true)
         }
 
-        if (call.method == "registerBroadcastReceiver") {
+        if (call.method == REGISTER_BROADCAST_RECEIVER) {
             bluetoothAdapter.registerBroadcastReceiver(activity)
             result.success(true)
         }
 
-        if (call.method == "unregisterBroadcastReceiver") {
+        if (call.method == UNREGISTER_BROADCAST_RECEIVER) {
             bluetoothAdapter.unregisterBroadcastReceiver(activity)
             result.success(true)
         }
@@ -102,6 +103,22 @@ class MethodChannelBluetoothAdapter : FlutterPlugin, MethodCallHandler, Activity
 
     override fun onDetachedFromActivity() {
         bluetoothAdapter.unregisterBroadcastReceiver(activity)
+    }
+
+    companion object {
+        private const val BLUETOOTH_ADAPTER = "bluetooth_adapter"
+        private const val INIT_BLUETOOTH_ADAPTER = "initBluetoothAdapter"
+        private const val CHECK_PERMISSION = "checkPermission"
+        private const val IS_ENABLE_BLUETOOTH = "isEnableBluetooth"
+        private const val IS_DISCOVERY_DEVICE = "isDiscoveryDevice"
+        private const val ENABLE_BLUETOOTH = "enableBluetooth"
+        private const val START_DISCOVERY_DEVICE = "startDeviceDiscovery"
+        private const val STOP_DISCOVERY_DEVICE = "stopDeviceDiscovery"
+        private const val LIST_NEW_DEVICES = "listNewDevices"
+        private const val LIST_PAIRED_DEVICES = "listPairedDevices"
+        private const val CALL_PAIRED_DEVICES = "callPairedDevices"
+        private const val REGISTER_BROADCAST_RECEIVER = "registerBroadcastReceiver"
+        private const val UNREGISTER_BROADCAST_RECEIVER = "unregisterBroadcastReceiver"
     }
 
 }
